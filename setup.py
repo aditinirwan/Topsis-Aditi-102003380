17f57ef13578d694046058cf28bdf86b3bce1e7b
aditinirwan <72340988+aditinirwan@users.noreply.github.com>
2023-01-22 14:20:56 +0530

    Updated

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 from distutils.core import setup
 
 setup(
-  name = 'Topsis-Aditi-102003380',         # How you named your package folder (MyLib)
-  packages = ['Topsis-Aditi-102003380'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'A Python package to find TOPSIS for multi-criteria decision analysis method',   # Give a short description about your library
-  author = 'ADITI NIRWAN',                   # Type in your name
-  author_email = 'anirwan_be20@thapar.edu',      # Type in your E-Mail
-  url = 'https://github.com/aditinirwan/Topsis-Aditi-102003380',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['Topsis', 'Data Science', 'UCS654'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  name = 'Topsis-Aditi-102003380',         
+  packages = ['Topsis-Aditi-102003380'],  
+  version = '0.1',      
+  license='MIT',        
+  description = 'A Python package to find TOPSIS for multi-criteria decision analysis method',   
+  author = 'ADITI NIRWAN',                   
+  author_email = 'anirwan_be20@thapar.edu',      
+  url = 'https://github.com/aditinirwan/Topsis-Aditi-102003380',   
+  download_url = 'https://github.com/aditinirwan/Topsis-Aditi-102003380/archive/refs/tags/v_01.tar.gz',   
+  keywords = ['Topsis', 'Data Science', 'UCS654'],   
+  install_requires=[            
           'pandas', 'numpy'
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
     'Topic :: Software Development :: Build Tools',
     'Operating System :: Microsoft :: Windows :: Windows 10',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
